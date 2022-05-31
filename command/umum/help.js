20f6aeda1850856244ecc3611f8ef4479bc388e6
Rama76 <80687852+rasssya76@users.noreply.github.com>
2022-05-31 15:42:38 +0700

    Add files via upload

--- command/umum/help.js
+++ command/umum/help.js
@@ -10,14 +10,12 @@ module.exports = {
 			const { command, prefix } = map;
 			const cmd = command.get(name) || [...command.values()].find((x) => x.alias.find((x) => x == args[0]));
 			if (!cmd || (cmd.category === "private" && !config.owner.includes(msg.sender)))
-				return await msg.reply("Command tidak ditemukan");
-			else data.push(`*Nama:* ` + cmd.name);
+				return await msg.reply("Command not found");
+			else data.push(`*Name:* ` + cmd.name);
 			if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(", ")}`);
 			if (cmd.desc) data.push(`*Deskripsi:* ${cmd.desc}`);
 			if (cmd.use)
-				data.push(
-					`*Penggunaan:* ${prefix}${cmd.name} ${cmd.use}\n\nCatatan: [] = opsional, | = atau, <> = harus diisi`
-				);
+				data.push(`*Use:* ${prefix}${cmd.name} ${cmd.use}\n\nNote: [] = optional, | = or, <> = must be filled`);
 
 			return await msg.reply(data.join("\n"));
 		} else {
@@ -31,6 +29,7 @@ module.exports = {
 				if (!cmd) continue;
 				if (config.ignore.directory.includes(info.category.toLowerCase())) continue;
 				cteg = info.category || "No Category";
+				if (info.type == "changelog") continue;
 				if (!cteg || cteg === "private") cteg = "owner command";
 				if (Object.keys(category).includes(cteg)) category[cteg].push(info);
 				else {
@@ -42,28 +41,27 @@ module.exports = {
 				"```" +
 				config.namebot +
 				"```\n\n" +
-				`Halo, ${pushName === undefined ? sender.split("@")[0] : pushName}\n*Disini Daftar Commandnya*\n\n`;
+				`Halo, ${pushName === undefined ? sender.split("@")[0] : pushName}\n*Here is the Command List*\n\n`;
 			const keys = Object.keys(category);
 			//var a = 1
 			for (const key of keys) {
-				str += `==== [ *${key.toUpperCase()}* ] ====\n${category[key]
+				str += `_*${key.toUpperCase()}*_ \n${category[key]
 					.map(
 						(cmd, index) =>
 							`*${index + 1}.* *${cmd.options.noPrefix ? "" : "#"}${cmd.name}*${
 								cmd.alias[0]
 									? "\n" +
 									  cmd.alias
-											.map((a) => (a ? `*• ${cmd.options.noPrefix ? "" : "#"}${a}*` : ""))
+											.map((a) => (a ? `*♣ ${cmd.options.noPrefix ? "" : "#"}${a}*` : ""))
 											.join("\n")
 									: ""
-							}\n*Example:* _${cmd.options.noPrefix ? "" : "#"}${cmd.name}${
+							}\n*Usage:* _${cmd.options.noPrefix ? "" : "#"}${cmd.name}${
 								cmd.category == "private" ? "" : cmd.use ? " " + cmd.use : ""
 							}_`
 					)
 					.join("\n\n")}\n\n`;
 			}
 			str += `typing *${prefix}help sticker* for get the details and example use`;
-		   //conn.sendMessage(msg.from,{react:{text:"✅️",key:msg.key}})
 			await conn.sendMessage(
 				msg.from,
 				{
@@ -72,11 +70,11 @@ module.exports = {
 					caption: str,
 					footer: config.namebot + " • " + config.ownername,
 					templateButtons: [
-						{ urlButton: { displayText: "GC BOT", url: "http://bit.ly/Bot-Wa" } },
-						//{ urlButton: { displayText: "Downloader", url: "https://downloader.rzkyfdlh.tech" } },
-						{ quickReplyButton: { displayText: "SC BOT", id: "#script" } },
-						{ quickReplyButton: { displayText: "OWNER", id: "#owner" } },
-						{ quickReplyButton: { displayText: "Dashboard", id: "#db" } },
+						{ urlButton: { displayText: "YT OWNER", url: "https://youtube.com/c/RamaGans" } },
+						{ urlButton: { displayText: "BOT GRUP", url: "http://bit.ly/Bot-Wa" } },
+						//{ quickReplyButton: { displayText: "Script Bot📑", id: "#script" } },
+						//{ quickReplyButton: { displayText: "LOGS", id: "#changelog" } },
+						{ quickReplyButton: { displayText: "DASHBOARD", id: "#db" } },
 					],
 				},
 				{ quoted: msg }
